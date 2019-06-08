3c114feb3cb48858cd93475e47ee969ed69b0276
coolbinay <binay.ens@gmail.com>
2019-06-08 17:38:14 +0530

    08 june migration update

--- rozzer/travello/views.py
+++ rozzer/travello/views.py
@@ -11,15 +11,18 @@ def index(request):
     dest1.img = 'destination_1.jpg'
     dest1.desc = 'The city that never sleeps'
     dest1.price = '800'
+    dest1.offer = False
 
     dest2.name = 'Delhi'
     dest2.img = 'destination_2.jpg'
     dest2.desc = 'The city that have no times'
     dest2.price = '600'
+    dest2.offer = True
 
     dest3.name = 'Bangalure'
     dest3.img = 'destination_3.jpg'
     dest3.desc = 'The city to enjoy!!!!!'
     dest3.price = '900'
+    dest3.offer = False
     dests = [dest1, dest2, dest3]
     return render(request, 'index.html', {'dests' : dests})
